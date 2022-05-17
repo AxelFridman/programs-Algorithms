c01f2e9fcc99eaf4843694835bba5d2de31fafb3
AxelFridman <fridman.axel@gmail.com>
2022-05-17 18:29:30 -0300

    agregada clase heao

--- graphSearch/main.cpp
+++ graphSearch/main.cpp
@@ -143,8 +143,18 @@ void Heap<tipo1>::agregarElemento(const tipo1 &elem) {
     }
 }
 
+
+
+
 void inicializarFuenteUnica(map<pair<int, int>, int> &grafo, int tamanioGrafo, vector<pair<int, int>> &s, int nodoInicial){
     for (int i = 0; i < tamanioGrafo; ++i) {
+        s[i] = make_pair(1147483647, -1); // todos los nodos al empezar tienen dis infinita al inicial y no tienen padre
+    }
+    s[nodoInicial] = make_pair(0, -1); // mienteas que el inicial tiene distancia 0, y tampoco tiene padre
+}
+
+void inicializarFuenteUnicaGrafoListaAdyacencia(vector<vector<vector<int>>> &grafo, vector<pair<int, int>> &s, int nodoInicial){
+    for (int i = 0; i < grafo.size(); ++i) {
         s[i] = make_pair(1147483647, -1); // todos los nodos al empezar tienen dis infinita al inicial y no tienen padre
     }
     s[nodoInicial] = make_pair(0, -1); // mienteas que el inicial tiene distancia 0, y tampoco tiene padre
@@ -155,6 +165,12 @@ void relajarArista(int u, int v, map<pair<int, int>, int> &grafo, vector<pair<in
         if(s[v].first>s[u].first+grafo[make_pair(u,v)]){ // si el costo en disancia de llegar a v es MAYOR que el de ir a u mas el de la arista UV.
             s[v]= make_pair(s[u].first +grafo[make_pair(u,v)], u); // actualizo nuevo camino para ir a V
         }
+    }
+}
+
+void relajarAristaListaAdyacencia(int u, int v, int k, vector<vector<vector<int>>> &grafo, vector<pair<int, int>> &s){
+    if(s[v].first>s[u].first+k){ // si el costo en disancia de llegar a v es MAYOR que el de ir a u mas el de la arista UV.
+        s[v]= make_pair(s[u].first +k, u); // actualizo nuevo camino para ir a V
     }
 }
 
@@ -178,40 +194,74 @@ bool bellmanFord(map<pair<int, int>, int> &grafo, int tamanioGrafo, vector<pair<
     return true;
 
 }
-
-void dijkstra(map<pair<int, int>, int> &grafo, int tamanioGrafo, vector<pair<int, int>> &s, int nodoInicial){
-    inicializarFuenteUnica(grafo,  tamanioGrafo, s,  nodoInicial);
-    vector<int> definitivos(0);
-
+bool bellmanFordListaAdyacencia(vector<vector<vector<int>>> &grafo, vector<pair<int, int>> &s, int nodoInicial) {
+    inicializarFuenteUnicaGrafoListaAdyacencia(grafo, s, nodoInicial);
+    for (int veces = 0; veces < grafo.size(); ++veces) {
+        for (int i = 0; i < grafo.size(); ++i) {
+            for (int j = 0; j < grafo[j].size(); ++j) {
+                int u = grafo[i][j][0];
+                int v = grafo[i][j][1];
+                int k = grafo[i][j][2];
+                relajarAristaListaAdyacencia(u, v, k, grafo, s);
+            }
+        }
+    }
+    // fijarme si tiene ciclos negativos:
+    for (int i = 0; i < grafo.size(); ++i) {
+        for (int j = 0; j < grafo[j].size(); ++j) {
+            int u = grafo[i][j][0];
+            int v = grafo[i][j][1];
+            int k = grafo[i][j][2];
+            if (s[v].first > s[u].first + k) {
+                return false;
+            }
+        }
+    }
+    return true;
 }
-int main() {
-    Heap<int> h1 = Heap<int>();
-    vector<int> v = {1,2,3,4,5,6,7};
-    h1.makeHeapFromVector(v);
-    h1.agregarElemento(8);
-    h1.agregarElemento(9);
-    h1.agregarElemento(10);
 
-    h1.desencolar();
-    h1.desencolar();
-    h1.desencolar();
 
-    map<pair<int, int>, int> grafo = {
-            {make_pair(0,1), -10},
-            {make_pair(1,2), 5},
-            {make_pair(2,3), 15},
-            {make_pair(3,0), 10},
-            {make_pair(2,0), 7},
-            {make_pair(0,5), -3},
-            {make_pair(5,4), -1},
-            {make_pair(4,3), -5}
+    void dijkstra(map<pair<int, int>, int> &grafo, int tamanioGrafo, vector<pair<int, int>> &s, int nodoInicial) {
+        inicializarFuenteUnica(grafo, tamanioGrafo, s, nodoInicial);
+        vector<int> definitivos(0);
+    }
 
-    };
-    int tamanioGrafo = 8;
-    vector<pair<int,int>> s(tamanioGrafo);
-    int nodoInicial = 5;
+    int main() {
+        /*
+        Heap<int> h1 = Heap<int>();
+        vector<int> v = {1, 2, 3, 4, 5, 6, 7};
+        h1.makeHeapFromVector(v);
+        h1.agregarElemento(8);
+        h1.agregarElemento(9);
+        h1.agregarElemento(10);
 
-    cout << bellmanFord(grafo, tamanioGrafo, s, nodoInicial);
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
-}
+        h1.desencolar();
+        h1.desencolar();
+        h1.desencolar();
+
+        map<pair<int, int>, int> grafo = {
+                {make_pair(0, 1), -10},
+                {make_pair(1, 2), 5},
+                {make_pair(2, 3), 15},
+                {make_pair(3, 0), 10},
+                {make_pair(2, 0), 7},
+                {make_pair(0, 5), -3},
+                {make_pair(5, 4), -1},
+                {make_pair(4, 3), -5}
+
+        };
+        int tamanioGrafo = 8;
+         */
+        vector<vector<vector<int>>> grafoListaAdyacencia = {
+                {{0, 1, 4},  {0, 2, 3}},
+                {{1, 0, -1}, {1, 2, -1}},
+                {{2, 1, 2},  {2, 0, 5}}
+        };
+
+        vector<pair<int, int>> s(grafoListaAdyacencia.size());
+        int nodoInicial = 2;
+        bellmanFordListaAdyacencia(grafoListaAdyacencia, s, nodoInicial);
+        cout << bellmanFordListaAdyacencia(grafoListaAdyacencia, s, nodoInicial);
+        std::cout << "Hello, World!" << std::endl;
+        return 0;
+    }
